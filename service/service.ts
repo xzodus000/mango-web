1fa3b8b18e349549a697007e79b18cbf0d1b45a2
xzodus000 <phanuphong.suthawas@gmail.com>
2024-10-07 00:49:58 +0700

    update url

--- service/service.ts
+++ service/service.ts
@@ -2,8 +2,7 @@ import axios from "axios";
 
 export const predictMangoService = async (filePath: any) => {
   try {
-    const url =
-      "https://mango-classification-flask-api-433919d0afaa.herokuapp.com/upload";
+    const url = "http://5.183.8.75:5000/upload";
     // const url = "http://localhost:5000/upload";
 
     const formData = new FormData();
